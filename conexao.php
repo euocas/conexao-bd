fe02de9478616d4a0d0416db468ecfe88f8a2a3f
Douglas Castro <castroctt@gmail.com>
2026-05-06 19:18:23 -0300

    first commit

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$banco = "biblioteca";
+$usuario = "root";
+$senha = "usbw";
+
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $usuario, $senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);   
+} catch (PDOException $e) {
+    die("Erro na conexão: " .$e->getMessage());
+}
+
+?>
